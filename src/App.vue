ecfd7760821c7298fc562a346a26a49baeed1b57
Cristian Peralta <cbperaltasegura@gmail.com>
2018-03-05 11:05:26 -0500

    add random joke in textarea

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,31 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
+    <textarea name="name" rows="8" cols="80" v-model="joke"></textarea>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data () {
+    return {
+      source: 'http://api.icndb.com/jokes',
+      joke: ''
+    }
+  },
+  created () {
+    this.getRandomJoke()
+  },
+  methods: {
+    getRandomJoke () {
+      this.$http.get(this.source + '/random').then((response) => {
+        let data = response.body
+        if (data.type === 'success') {
+          this.joke = data.value.joke
+        }
+      })
+    }
   }
 }
 </script>
